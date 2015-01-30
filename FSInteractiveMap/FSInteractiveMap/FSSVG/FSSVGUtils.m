500025ddd4e12a5d20a40acb9f6104bf9c491e7e
arthur guibert <arthur.guibert@centraliens-lille.org>
2015-01-30 22:46:49 +0000

    FSSVGUtils class refactored

--- FSInteractiveMap/FSInteractiveMap/FSSVG/FSSVGUtils.m
+++ FSInteractiveMap/FSInteractiveMap/FSSVG/FSSVGUtils.m
@@ -32,55 +32,72 @@
     CGAffineTransform tranform = CGAffineTransformIdentity;
     
     if([str length] > 0) {
-        // Only supporting translate and matrix so far...
-        NSRange range = NSMakeRange(0, [str length]);
-        NSString* patternTranslate = @"translate\\((.*)\\)";
-        NSString* patternMatrix = @"matrix\\((.*)\\)";
-        NSError* regexError = nil;
-        
-        NSRegularExpression* regexTranslate = [NSRegularExpression regularExpressionWithPattern:patternTranslate
-                                                                                        options:NSRegularExpressionCaseInsensitive
-                                                                                          error:&regexError];
-        
-        if(!regexError) {
-            NSArray* matches = [regexTranslate matchesInString:str
-                                                       options:NSMatchingWithoutAnchoringBounds
-                                                         range:range];
-            if([matches count] > 0) {
-                NSTextCheckingResult *entry = matches[0];
-                NSString *parameters = [str substringWithRange:[entry rangeAtIndex:1]];
-                NSArray* coordinates = [FSSVGUtils parsePoints:[parameters UTF8String]];
-                
-                if([coordinates count] == 2) {
-                    tranform = CGAffineTransformMakeTranslation([coordinates[0] floatValue], [coordinates[1] floatValue]);
-                    return tranform;
-                }
-            }
-        }
-        
-        NSRegularExpression* regexMatrix = [NSRegularExpression regularExpressionWithPattern:patternMatrix
-                                                                                     options:NSRegularExpressionCaseInsensitive
-                                                                                       error:&regexError];
-        
-        if(!regexError) {
-            NSArray* matches = [regexMatrix matchesInString:str
-                                                    options:NSMatchingWithoutAnchoringBounds
-                                                      range:range];
-            if([matches count] > 0) {
-                NSTextCheckingResult *entry = matches[0];
-                NSString *parameters = [str substringWithRange:[entry rangeAtIndex:1]];
-                NSArray* coordinates = [FSSVGUtils parsePoints:[parameters UTF8String]];
-                
-                if([coordinates count] == 6) {
-                    tranform = CGAffineTransformMake([coordinates[0] floatValue], [coordinates[1] floatValue], [coordinates[2] floatValue], [coordinates[3] floatValue], [coordinates[4] floatValue], [coordinates[5] floatValue]);
-                    return tranform;
-                }
+        tranform = CGAffineTransformConcat(tranform, [self parseMatrix:str]);
+        tranform = CGAffineTransformConcat(tranform, [self parseTranslate:str]);
+    }
+    
+    return tranform;
+}
+
++ (CGAffineTransform)parseMatrix:(NSString*)str
+{
+    CGAffineTransform tranform = CGAffineTransformIdentity;
+    NSRange range = NSMakeRange(0, [str length]);
+    NSString* patternMatrix = @"matrix\\((.*)\\)";
+    NSError* regexError = nil;
+    
+    NSRegularExpression* regexMatrix = [NSRegularExpression regularExpressionWithPattern:patternMatrix
+                                                                                 options:NSRegularExpressionCaseInsensitive
+                                                                                   error:&regexError];
+    
+    if(!regexError) {
+        NSArray* matches = [regexMatrix matchesInString:str
+                                                options:NSMatchingWithoutAnchoringBounds
+                                                  range:range];
+        if([matches count] > 0) {
+            // We have a match, lets parse the points
+            NSTextCheckingResult *entry = matches[0];
+            NSString *parameters = [str substringWithRange:[entry rangeAtIndex:1]];
+            NSArray* coordinates = [FSSVGUtils parsePoints:[parameters UTF8String]];
+            
+            if([coordinates count] == 6) {
+                // We need a 3x3 matrix
+                tranform = CGAffineTransformMake([coordinates[0] floatValue], [coordinates[1] floatValue], [coordinates[2] floatValue], [coordinates[3] floatValue], [coordinates[4] floatValue], [coordinates[5] floatValue]);
             }
         }
     }
     
+    return tranform;
+}
+
++ (CGAffineTransform)parseTranslate:(NSString*)str
+{
+    CGAffineTransform tranform = CGAffineTransformIdentity;
+    NSRange range = NSMakeRange(0, [str length]);
+    NSString* patternTranslate = @"translate\\((.*)\\)";
+    NSError* regexError = nil;
     
-    return CGAffineTransformIdentity;
+    NSRegularExpression* regexTranslate = [NSRegularExpression regularExpressionWithPattern:patternTranslate
+                                                                                    options:NSRegularExpressionCaseInsensitive
+                                                                                      error:&regexError];
+    
+    if(!regexError) {
+        NSArray* matches = [regexTranslate matchesInString:str
+                                                   options:NSMatchingWithoutAnchoringBounds
+                                                     range:range];
+        if([matches count] > 0) {
+            // We have a match, lets parse the points
+            NSTextCheckingResult *entry = matches[0];
+            NSString *parameters = [str substringWithRange:[entry rangeAtIndex:1]];
+            NSArray* coordinates = [FSSVGUtils parsePoints:[parameters UTF8String]];
+            
+            if([coordinates count] == 2) {
+                tranform = CGAffineTransformMakeTranslation([coordinates[0] floatValue], [coordinates[1] floatValue]);
+            }
+        }
+    }
+
+    return tranform;
 }
 
 @end
